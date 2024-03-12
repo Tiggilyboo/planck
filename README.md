a128d0d6922e046e73d225edbf9a09aff135f613
Simon Willshire <me@simonwillshire.com>
2024-03-12 22:20:47 +0100

    WIP towards defining key layout with RMK

--- README.md
+++ README.md
@@ -1,4 +1,34 @@
-# Planck RP2040 BLE Keyboard
+# RMK
 
+Keyboard firmware for cortex-m, with layer/dynamic keymap/[vial](https://get.vial.today) support, written in Rust.
 
+## Checklist after generating the firmware project
 
+- [X] Update `memory.x` for your microcontroller(if needed)
+
+- [X] Create your `vial.json` by your layout: https://get.vial.today/docs/porting-to-via.html, replace the default one
+
+- [ ] Update your default keymap at `src/keymap.rs`
+
+- [ ] Update USB interrupt binding at `src/main.rs`(if needed)
+
+- [ ] Check the chip name of `probe-rs` is right, if you don't use `cargo run`, you can skip this step
+
+## For BLE only
+
+To use NRF BLE, you should have [nrf s140 softdevice 7.3.0](https://www.nordicsemi.com/Products/Development-software/s140/download) flashed to nrf52840 first. 
+
+The following are the detailed steps:
+
+1. Erase the flash:
+   ```shell
+   probe-rs erase --chip nrf52840_xxAA
+   ```
+2. Flash softdevice firmware to flash:
+   ```shell
+   probe-rs download --verify --format hex --chip nRF52840_xxAA s140_nrf52_7.3.0_softdevice.hex
+   ```
+3. Compile, flash and run the example
+   ```shell
+   cargo run --release
+   ```
---
+++ src/main.rs
@@ -0,0 +1,98 @@
+#![no_main]
+#![no_std]
+
+#[macro_use]
+mod keymap;
+#[macro_use]
+mod macros;
+mod vial;
+
+use crate::keymap::{COL, NUM_LAYER, ROW};
+use core::cell::RefCell;
+use defmt::*;
+use defmt_rtt as _;
+use embassy_executor::Spawner;
+use embassy_rp::{
+    bind_interrupts,
+    flash::{Blocking, Flash},
+    gpio::{AnyPin, Input, Output},
+    peripherals::{self, USB},
+    usb::{Driver, InterruptHandler},
+};
+use panic_probe as _;
+use rmk::{
+    config::{RmkConfig, VialConfig},
+    initialize_keyboard_with_config_and_run,
+    keymap::KeyMap,
+};
+use static_cell::StaticCell;
+use vial::{VIAL_KEYBOARD_DEF, VIAL_KEYBOARD_ID};
+
+bind_interrupts!(struct Irqs {
+    USBCTRL_IRQ => InterruptHandler<USB>;
+});
+
+const EEPROM_SIZE: usize = 128;
+const FLASH_SIZE: usize = 2 * 1024 * 1024;
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    info!("RMK start!");
+    // Initialize peripherals
+    let p = embassy_rp::init(Default::default());
+
+    // Create the usb driver, from the HAL
+    let driver = Driver::new(p.USB, Irqs);
+
+    // Pin config
+    let (input_pins, output_pins) = config_matrix_pins_rp!(
+        peripherals: p, 
+        input: [
+            PIN_4, PIN_5, PIN_6, PIN_7, 
+            PIN_8, PIN_9, PIN_10, PIN_11, 
+            PIN_12, PIN_13, PIN_14, PIN_15, 
+        ], 
+        output: [
+            PIN_18, PIN_19, PIN_20, PIN_21,
+        ]);
+
+    // Use internal flash to emulate eeprom
+    let flash = Flash::<_, Blocking, FLASH_SIZE>::new_blocking(p.FLASH);
+    // Keymap + eeprom config
+    static MY_KEYMAP: StaticCell<
+        RefCell<
+            KeyMap<
+                Flash<peripherals::FLASH, Blocking, FLASH_SIZE>,
+                EEPROM_SIZE,
+                ROW,
+                COL,
+                NUM_LAYER,
+            >,
+        >,
+    > = StaticCell::new();
+    let keymap = MY_KEYMAP.init(RefCell::new(KeyMap::new(
+        crate::keymap::KEYMAP,
+        Some(flash),
+        None,
+    )));
+
+    let vial_config = VialConfig::new(VIAL_KEYBOARD_ID, VIAL_KEYBOARD_DEF);
+
+    let keyboard_config = RmkConfig {
+        vial_config,
+        ..Default::default()
+    };
+
+    // Start serving
+    initialize_keyboard_with_config_and_run::<
+        Driver<'_, USB>,
+        Input<'_, AnyPin>,
+        Output<'_, AnyPin>,
+        Flash<peripherals::FLASH, Blocking, FLASH_SIZE>,
+        EEPROM_SIZE,
+        ROW,
+        COL,
+        NUM_LAYER,
+    >(driver, input_pins, output_pins, keymap, keyboard_config)
+    .await;
+}
