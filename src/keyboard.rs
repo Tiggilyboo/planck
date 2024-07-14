ddf0bcc24846abc3aadc05d9c8360538ebe14eb7
Simon Willshire <me@simonwillshire.com>
2024-07-14 11:21:29 +0200

    Move keystate to own module, bump deps

--- src/keyboard.rs
+++ src/keyboard.rs
@@ -6,6 +6,11 @@ use num_enum::FromPrimitive;
 pub mod keycodes;
 use keycodes::*;
 
+pub mod keystate;
+use keystate::*;
+
+pub mod ble;
+
 #[macro_export]
 macro_rules! define_keymap {
     ($cols:expr, $rows:expr, $layers:expr, [$([$([$($key: ident), +]), +]), +]) => {{
@@ -37,76 +42,6 @@ pub enum Layer {
     Lower = 0b01,
     Upper = 0b10,
     Both = 0b11,
-}
-
-#[derive(Copy, Clone)]
-pub struct KeyState {
-    pub changed: bool,
-    pub pressed: bool,
-    key_code: KeyCode,
-    debounce_counter: u16,
-}
-
-impl Default for KeyState {
-    fn default() -> Self {
-        Self::new(KeyCode::No)
-    }
-}
-
-impl KeyState {
-    pub fn new(kc: KeyCode) -> Self {
-        Self {
-            key_code: kc,
-            changed: false,
-            pressed: false,
-            debounce_counter: 0,
-        }
-    }
-
-    pub fn toggle_pressed(&mut self) {
-        self.pressed = !self.pressed;
-    }
-
-    pub fn get_key_code(&self) -> KeyCode {
-        self.key_code
-    }
-
-    pub fn set_key_code(&mut self, kc: KeyCode) {
-        self.key_code = kc;
-    }
-
-    fn debounce_counter_add(&mut self, delta_ms: u16) {
-        if u16::MAX - self.debounce_counter <= delta_ms {
-            self.debounce_counter = u16::MAX;
-        } else {
-            self.debounce_counter += 1;
-        }
-    }
-    
-    fn debounce_counter_sub(&mut self, delta_ms: u16) {
-        if delta_ms > self.debounce_counter {
-            self.debounce_counter = 0;
-        } else {
-            self.debounce_counter -= 1;
-        }
-    }
-
-    pub fn detect_change(&mut self, pin_state: bool, delta_ms: u16) -> bool {
-        if delta_ms > 0 {
-            if self.pressed == pin_state {
-                self.debounce_counter_sub(delta_ms);
-            } else {
-                if self.debounce_counter < 100 {
-                    self.debounce_counter_add(delta_ms);
-                } else {
-                    self.debounce_counter = 0;
-                    return true;
-                }
-            }
-        }
-
-        return false;
-    }
 }
 
 pub struct Keyboard<'a, const COLS: usize, const ROWS: usize, const LAYERS: usize> {
@@ -180,13 +115,12 @@ impl <'a, const COLS: usize, const ROWS: usize, const LAYERS: usize> Keyboard<'a
             // process as normal
             _ => {
                 if pressed {
-                    log::info!("{:?} is down", key_code);
+                    //log::info!("{:?} is down", key_code);
                     if key_code.is_modifier() {
                         report.modifier |= key_code.as_modifier_bit();
                     } else if let Some(i) = report.keycodes.iter().position(|&k| k == 0) {
                         report.keycodes[i] = key_code as u8
                     }
-                
                 } else {
                     if key_code.is_modifier() {
                         report.modifier &= !key_code.as_modifier_bit();
@@ -197,8 +131,6 @@ impl <'a, const COLS: usize, const ROWS: usize, const LAYERS: usize> Keyboard<'a
                 true
             }
         }
-        
-        
     }
 
     pub async fn scan(&mut self) -> bool {
